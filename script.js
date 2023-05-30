551059af1312e7546db1234f1f74472a0f081dd5
Josef8852 <106317172+Josef8852@users.noreply.github.com>
2023-05-30 19:59:16 +0200

    Use Structured Clone

--- script.js
+++ script.js
@@ -1,58 +1,61 @@
-'use strict' ; 
+'use strict';
 
-let container = document.querySelector('.cards') ; 
+let container = document.querySelector('.cards');
 
-let flags = document.querySelector('.flags') ; 
+let flags = document.querySelector('.flags');
 
-let darkMode = document.querySelector('.darkMode') ; 
+let darkMode = document.querySelector('.darkMode');
 
-let nav = document.querySelector('.nav') ; 
+let nav = document.querySelector('.nav');
 
 let select = document.querySelector('.select')
 
-let options = document.querySelectorAll('.opt') ; 
+let options = document.querySelectorAll('.opt');
 
-let input = document.querySelector('.field') ; 
+let input = document.querySelector('.field');
 
-let icon = document.querySelector('.fa-magnifying-glass')  ;
+let icon = document.querySelector('.fa-magnifying-glass');
 
-let detailsContainer = document.querySelector('.details') ; 
+let detailsContainer = document.querySelector('.details');
 
-let detailsSection = document.querySelector('.fullDetails') ;
+let detailsSection = document.querySelector('.fullDetails');
 
 
-
+let clone ; 
 
 
 
 let getData = async () => {
-try {
-  let response = await fetch('https://restcountries.com/v3.1/all') ;
+  try {
+    let response = await fetch('https://restcountries.com/v3.1/all');
 
 
-  let data = await response.json() ; 
+    let data = await response.json();
+    
+    clone = structuredClone(data) ; 
 
-  return data ; 
-}
-catch (err) {
-  let HTML = `<p class="error"> Something went Wrong⛔ </p>` ; 
+    return data;
+  }
+  catch (err) {
+    let HTML = `<p class="error"> Something went Wrong⛔ </p>`;
 
-  container.insertAdjacentHTML('afterbegin' , HTML) ;
+    container.insertAdjacentHTML('afterbegin', HTML);
 
-  throw err;
-}
+    throw err;
+  }
 
 };
 
 
-  let countries = [] ,  cards  ; 
+
+let countries = [], cards;
 
 
 
 let showData = () => {
-  detailsSection.classList.add('hide') ;
+  detailsSection.classList.add('hide');
   getData().then((data) => {
-    countries = data.map((element, index) => { 
+    countries = data.map((element, index) => {
       let HTML = `
       <div class="card">
       <div class="image">
@@ -67,111 +70,111 @@ let showData = () => {
       </div>
     
     </div>
-      ` ; 
-       container.insertAdjacentHTML('beforeend' , HTML)  ;
-       cards = document.querySelectorAll('.card') ;
-      return {countryName : element.name.common , card :  container.children[index], region : element.region}
-     });
+      ` ;
+      container.insertAdjacentHTML('beforeend', HTML);
+      cards = document.querySelectorAll('.card');
+      return { countryName: element.name.common, card: container.children[index], region: element.region }
+    });
   });
 
 }
 
 
-showData() ;
-  
+showData();
 
-let changeStyle = (collection , className) => {
+
+let changeStyle = (collection, className) => {
   collection.forEach((element) => {
-    element.classList.toggle(`${className}`) ; 
+    element.classList.toggle(`${className}`);
   })
-  }
+}
 
 /* Dark mode  */
 
-darkMode.addEventListener('click' , () => {
-document.body.classList.toggle('dark-bg') ; 
+darkMode.addEventListener('click', () => {
+  document.body.classList.toggle('dark-bg');
 
-document.body.classList.toggle('light-text') ;
+  document.body.classList.toggle('light-text');
 
-nav.classList.toggle('dark-bg') ; 
+  nav.classList.toggle('dark-bg');
 
-select.classList.toggle('dark-bg') ;
+  select.classList.toggle('dark-bg');
 
-select.classList.toggle('light-text') ; 
+  select.classList.toggle('light-text');
 
-input.classList.toggle('dark-bg') ;
+  input.classList.toggle('dark-bg');
 
-input.classList.toggle('light-text') ;
+  input.classList.toggle('light-text');
 
-icon.classList.toggle('light-text') ;
+  icon.classList.toggle('light-text');
 
-changeStyle(options , 'dark-bg') ; 
+  changeStyle(options, 'dark-bg');
 
-changeStyle(options , 'light-text') ; 
+  changeStyle(options, 'light-text');
 
-}) ; 
+});
 
 
 /* Search */
 
-input.addEventListener('input' , (e) => {
-  let value = e.target.value ; 
+input.addEventListener('input', (e) => {
+  let value = e.target.value;
   countries.forEach((element) => {
-    let isEqual = element.countryName.includes(value) ; 
-    element.card.classList.toggle('hide' , !isEqual) ; 
-  }) ;
+    let isEqual = element.countryName.includes(value);
+    element.card.classList.toggle('hide', !isEqual);
+  });
 });
 
 
 /* Filter by Region */
 
-select.addEventListener('change' , (e) => {
-let value = e.target.value ; 
-countries.forEach((element) => {
-  let isEqual = element.region === value || value === 'All'  ? true : false ; 
-  element.card.classList.toggle('hide' , !isEqual) ; 
-});
+select.addEventListener('change', (e) => {
+  let value = e.target.value;
+  countries.forEach((element) => {
+    let isEqual = element.region === value || value === 'All' ? true : false;
+    element.card.classList.toggle('hide', !isEqual);
+  });
 
 });
 
 
 let hide = () => {
-  input.classList.add('hide') ; 
-  select.classList.add('hide') ;
-  icon.classList.add('hide') ; 
-  flags.classList.add('hide') ; 
+  input.classList.add('hide');
+  select.classList.add('hide');
+  icon.classList.add('hide');
+  flags.classList.add('hide');
   detailsSection.classList.add('show')
 }
 
 let show = () => {
-  input.classList.remove('hide') ; 
-  select.classList.remove('hide') ;
-  icon.classList.remove('hide') ; 
-  flags.classList.remove('hide') ; 
-  detailsSection.classList.remove('show') ; 
+  input.classList.remove('hide');
+  select.classList.remove('hide');
+  icon.classList.remove('hide');
+  flags.classList.remove('hide');
+  detailsSection.classList.remove('show');
 }
 
 
 let getKeys = (obj) => {
-  let arr = [] ; 
+  let arr = [];
   let keys = Object.keys(obj);
   for (let i = 0; i < keys.length; i++) {
-  let val = obj[keys[i]];
-  arr.push(val) ; 
-}
-return arr ; 
+    let val = obj[keys[i]];
+    arr.push(val);
+  }
+  return arr;
 }
 
 
 let getFullDetails = (element) => {
- let topLevelDomain = element.tld === undefined ? 'Does not Exist' : element.tld ; 
+  let topLevelDomain = element.tld === undefined ? 'Does not Exist' : element.tld;
 
- let subRegion = element.subregion === undefined ? 'Does not Exist' : element.subregion ; 
+  let subRegion = element.subregion === undefined ? 'Does not Exist' : element.subregion;
 
- let currency = element.currencies === undefined ? 'No Currency' : element.currencies[Object.keys(element.currencies)[0]].name  ;
+  let currency = element.currencies === undefined ? 'No Currency' : element.currencies[Object.keys(element.currencies)[0]].name;
 
 
-let HTML = `
+  let HTML = `
 <div>
 <button class="removeDetails"><i class="fa-solid fa-arrow-left"></i> Back</button>
 <div class="image">
@@ -179,7 +182,7 @@ let HTML = `
   <img src="${element.flags.png}" alt="">
 </div>
 <div class="text">
-  <p>Native Name: ${element.altSpellings[element.altSpellings.length-1]}</p>
+  <p>Native Name: ${element.altSpellings[element.altSpellings.length - 1]}</p>
   <p>Population: ${element.population} </p>
   <p>Region: ${element.region}</p>
   <p>Sub Region: ${subRegion} </p>
@@ -192,20 +195,20 @@ let HTML = `
 `;
 
 
-  detailsContainer.insertAdjacentHTML('afterbegin', HTML) ; 
+  detailsContainer.insertAdjacentHTML('afterbegin', HTML);
 
 
-/* Close Button */
-setTimeout(() => {
-  let button = document.querySelector('.removeDetails') ; 
- 
-   button.addEventListener('click' , () => {
-      detailsContainer.removeChild(detailsContainer.firstElementChild) ; 
-      show() ;
-   });
- 
- 
- } ,100);
+  /* Close Button */
+  setTimeout(() => {
+    let button = document.querySelector('.removeDetails');
+
+    button.addEventListener('click', () => {
+      detailsContainer.removeChild(detailsContainer.firstElementChild);
+      show();
+    });
+
+
+  }, 100);
 
 
 
@@ -213,19 +216,19 @@ setTimeout(() => {
 
 
 let getSelectedElement = (clickedElement) => {
-let clickedName = clickedElement.children[1].children[0].textContent ; 
-hide()  ;
 
-getData().then((data) => {
- 
-data.forEach((element) => {
-  if(element.name.common === clickedName) {
-    getFullDetails(element) ; 
-  }
+  let clickedName = clickedElement.children[1].children[0].textContent;
+  hide();
 
-}) ; 
 
-});
+
+    clone.forEach((element) => {
+      if (element.name.common === clickedName) {
+        getFullDetails(element);
+      }
+
+    });
+
 
 }
 
@@ -233,15 +236,14 @@ data.forEach((element) => {
 
 
 setTimeout(() => {
-cards = document.querySelectorAll('.card') ; 
+  cards = document.querySelectorAll('.card');
+  cards.forEach((element) => {
+    element.addEventListener('click', () => {
+      getSelectedElement(element);
+    });
+  })
 
-cards.forEach((element) => {
-  element.addEventListener('click' , () => {
-    getSelectedElement(element)  ;
-  });
-})
-
-} ,1000) ; 
+}, 1000);
 
 
 
